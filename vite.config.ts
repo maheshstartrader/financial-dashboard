f4beb4a1f6d58af8c2d166b0408499cc84aff820
maheshstartrader <mahesh.startrader@gmail.com>
2026-06-01 17:02:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // Let the template handle SSR automatically
+  plugins: [react()],
 });
